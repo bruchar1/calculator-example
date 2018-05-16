6ac5a6efa3efd92662025c2b90f4213689aea371
Charles Brunet <charles.brunet@optelgroup.com>
2018-05-16 16:29:09 -0400

    formatting

--- test_calculator.py
+++ test_calculator.py
@@ -12,17 +12,20 @@ def test_add(calculator):
     calculator.push('+')
     assert 2 == calculator.pop()
 
+
 def test_sub(calculator):
     calculator.push(1)
     calculator.push(1)
     calculator.push('-')
     assert 0 == calculator.pop()
 
+
 def test_mul(calculator):
     calculator.push(1)
     calculator.push(2)
     calculator.push('*')
     assert 2 == calculator.pop()
+
 
 def test_div(calculator):
     calculator.push(2)
